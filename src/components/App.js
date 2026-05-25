d92fe9fb44dba15bf4f71088b23131c067f483bd
NIKHIL PATEL <115173157+nikhilpatel9@users.noreply.github.com>
2026-05-25 22:40:23 +0530

    LASt

--- src/components/App.js
+++ src/components/App.js
@@ -1,20 +1,14 @@
-
-import React from "react";
-import './../styles/App.css';
+import React, { useState } from "react";
 import WeatherDisplay from "./WeatherDisplay";
 
 const App = () => {
- 
-   const [weather] = useState({ temperature: 25, conditions: "Sunny" });
-
- 
+  const [weather] = useState({ temperature: 25, conditions: "Sunny" });
 
   return (
     <div>
-      <WeatherDisplay weather={weather}/>
+      <WeatherDisplay weather={weather} />
     </div>
   );
+};
 
-}
-
-export default App
+export default App;
