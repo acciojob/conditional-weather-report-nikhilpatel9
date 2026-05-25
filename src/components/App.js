034a5773993adf1c9025750fe973de21c822d801
NIKHIL PATEL <115173157+nikhilpatel9@users.noreply.github.com>
2026-05-25 21:41:36 +0530

    Nikhil

--- src/components/App.js
+++ src/components/App.js
@@ -3,9 +3,13 @@ import React from "react";
 import './../styles/App.css';
 
 const App = () => {
+  const weather ={ temperature: 25, conditions: "Sunny" };
+  const isHot=weather.temperature>20;
+  const tempColor= { color: isHot ? "red" : "blue" };
   return (
     <div>
-        {/* Do not remove the main div */}
+        <p style={tempColor}>{weather.temperature}</p>
+        <span style={tempColor}>{weather.conditions}</span>
     </div>
   )
 }
